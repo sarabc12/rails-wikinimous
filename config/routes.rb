42bf5b69209228d22ec1549c5ae5e3d3dfc01a1c
sara burchi colonna <burchicolonnasara@gmail.com>
2025-03-04 16:40:06 +0100

    not finish Rails Wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "articles", to: "articles#index"
+
+  get "articles/:id", to: "articles#show", as: :article
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+10.times do
+  new_article = Article.new(
+    title: Faker::Movie.title,
+    content: Faker::Movie.quote
+  )
+  new_article.save
+end
